8214f77794ab1fa545398b1efbd5f369f61abc73
Gerardo Herrera <Gerardo Herrera@10.182.3.80>
2023-05-22 14:33:12 -0500

    Método de presentación

---
+++ Examen/src/models/TablaPeriodica.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Gerardo Herrera
+ */
+public class TablaPeriodica {
+    
+    
+    public static void author68612(){
+        System.out.println("Nombre: Gerardo Isidro Herrera Pacheco");
+        System.out.println("Carrera: Ingenieria en sistemas computacionales Grupo: 4B");
+        System.out.println("Matricula: 68612");
+        System.out.println("Correo: al68612@uacam.com");    
+    }
+    
+}
